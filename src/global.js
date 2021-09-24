7c0702cf8e28661c89c2d1bdd03d6d7d0f94ba5c
Md. Iftakher Ahmad <iftakhar.ahmed@therapservices.net>
2021-09-24 20:32:24 +0600

    dfsgsd

--- src/global.js
+++ src/global.js
@@ -1,7 +1,9 @@
-module.exports.BACKEND_URL="http://localhost:3002/";
+module.exports.BACKEND_URL="https://iaj-node-server23.herokuapp.com/";
 
 module.exports.USERNAME_KEY="UnivDhaKa:AeH";
 
 module.exports.PASSWORD_KEY="UnivDhaka:AEpAss";
 
 module.exports.ADMIN_KEY="ftghjkl76sysjfdsfsateuruuy3287gfbfjdsfiufy"
+
+
